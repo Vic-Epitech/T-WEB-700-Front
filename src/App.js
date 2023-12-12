2ae4f7a518f5ba761906976f1627397e99f265a7
Vic-rider <tossouvincentsonagnon@gmail.com>
2023-12-12 08:06:42 +0100

    add more routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Dashboard from './pages/dash/Dashboard';
 
 import './App.css'
 import './index.css'
+import Crypto from "./pages/Crypto";
 
 function App() {
   return (
@@ -16,12 +17,18 @@ function App() {
     <Routes>
     
       <Route path="/dash" element={<Dashboard/>} />
+      <Route path="/dash/cryptos" element={<Dashboard/>} />
+      <Route path="/dash/articles" element={<Dashboard/>} />
+      <Route path="/dash/users" element={<Dashboard/>} />
+      <Route path="/dash/settings" element={<Dashboard/>} />
+      <Route path="/dash/profile" element={<Dashboard/>} />
     
       <Route path="/auth/login" element={<Login/>} />
       <Route path="/auth/register" element={<Register/>} />
       <Route path="/auth/forgot-password" element={<ForgotPassword/>} />
 
       <Route path="/blog" element={<Blog/>} />
+      <Route path="/cryptos" element={<Crypto/>} />
       <Route path="/" element={<Home/>} />
 
     </Routes>
