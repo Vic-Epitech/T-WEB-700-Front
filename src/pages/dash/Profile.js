727b59a1a4f1c004499ace8e6c85985a918c5a59
Vic-rider <tossouvincentsonagnon@gmail.com>
2024-01-04 06:33:20 +0100

    add change user datas form & change password form

--- src/pages/dash/Profile.js
+++ src/pages/dash/Profile.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import * as React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
@@ -25,8 +27,9 @@ import './dash.css';
 import { Newspaper } from '@mui/icons-material';
 import { VerifiedUserTwoTone } from '@mui/icons-material';
 import { PowerOff } from '@mui/icons-material';
-import { corisXUserDatas, corisXUserToken } from '../../utils/utils';
+import { baseUrl, corisXUserDatas, corisXUserToken } from '../../utils/utils';
 import { Navigate } from 'react-router-dom';
+import axios from "axios";
 
 const drawerWidth = 240;
 
@@ -75,6 +78,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   justifyContent: 'flex-end',
 }));
 
+
 export default function Profile() {
 
   const theme = useTheme();
@@ -87,8 +91,24 @@ export default function Profile() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-  const userData = JSON.parse(localStorage.getItem(corisXUserDatas));
+  
 
+  const userData = JSON.parse(localStorage.getItem(corisXUserDatas));
+  const token = localStorage.getItem(corisXUserToken)
+  const [user, setUser] = useState([]);
+  const [userloader, setUserLoader] = useState(true);
+
+  const [firstName, setFirstName] = useState(userData.firstname);
+  const [lastName, setLastName] = useState(userData.lastname);
+  const [userName, setUserName] = useState(userData.username);
+  const [email, setEmail] = useState(userData.email);
+  
+  const [loadingUserDatas, setLoadingUserDatas] = useState(false);
+  
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+
+  const [loadingPasswordDatas, setLoadingPasswordDatas] = useState(false);
 
   if(!userData) {
     Navigate('/');
@@ -101,7 +121,89 @@ export default function Profile() {
     location.reload();
   };
 
+  const changeDatas = async () => {
+    
+    const log = {
+      "lastname": lastName,
+      "firstname": firstName,
+      "username": userName,
+      "email": email
+   }
+
+     let config = {
+       headers: {
+         'Authorization': 'Bearer ' + token
+       }
+     }
+
+     try {
+
+      setLoadingUserDatas(true);
+
+       const response = await axios.post( baseUrl + 'users/updateuserinfos', log, config);
+       console.log(response.data); // Handle successful login
+
+       if(response.data.data) {
+
+
+
+       }
+       else {
+         //   setError(true);
+       }
+
+     } catch (error) {
+       console.error('Login failed', error);
+     } finally {
+      setLoadingUserDatas(false);
+     }
+
+  };
+
+  const changePassword = () => {
+    // localStorage.removeItem(corisXUserDatas)
+    // localStorage.removeItem(corisXUserToken)
+    // // eslint-disable-next-line no-restricted-globals
+    // location.reload();
+  };
+
+
+  useEffect(() => {
+
+    // console.log(userData)
+
+    setUserLoader(true);
+
+    //  setLoader(true);
+     let config = {
+       headers: {
+         'Authorization': 'Bearer ' + token
+       }
+     }
+
+     fetch( baseUrl + 'users/user?username=' + userData.username , config)
+        .then((response) => response.json())
+        .then((data) => {
+           console.log(data);
+          //  totalPosts = data.data;
+          //  setUser(data.data);
+
+          // setLoader(false);
+        })
+        .catch((err) => {
+           console.log(err);
+           setUserLoader(false);
+        });
+  }, []);
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  
   return (
+    
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
@@ -224,7 +326,83 @@ export default function Profile() {
       <Main open={open}>
         <DrawerHeader />
         <Typography paragraph>
-          Profile
+
+          <div className="d-flex">
+
+            <img className="profile_logo" src={`https://api.dicebear.com/7.x/open-peeps/svg?seed=${userData.firstname}+${userData.lastname}`} alt="Logo" />
+
+            <h2 className="profile_name">{userData.firstname} {userData.lastname}</h2>
+
+          </div>
+
+          <div className="container">
+          
+            <div className="row gx-4">
+
+              <form className="col-md-6 form_container">
+
+                  <h4>Information de Compte</h4>
+
+                  <div className="form_login row gy-4">
+
+                      <div className="col-6">
+                          <input type="text" className=""  value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="Prénom"/>
+                      </div>
+
+                      <div className="col-6">
+                          <input type="text" className=""  value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder="Nom"/>
+                      </div>
+
+                      <div className="col-12">
+                          <input type="text" className="" disabled value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Username"/>
+                      </div>
+
+                      <div className="col-12">
+                          <input type="email" className="" disabled  value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email"/>
+                      </div>
+
+                  </div>
+
+                  <div className="form_login row actions">
+
+                      <div className="col-10 action">
+                          <input onClick={changeDatas} type="button" value={loadingUserDatas ? 'Changement d\'information...' : 'Valider'}/>
+                      </div>
+
+                  </div>
+
+              </form>
+
+              <form className="col-md-5 form_container ml-2rem">
+
+                  <h4>Changement de mot de passe</h4>
+
+                  <div className="form_login row gy-4">
+
+                      <div className="col-12">
+                          <input type="password" className=""  value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Mot de passe"/>
+                      </div>
+
+                      <div className="col-12">
+                          <input type="password" className=""  value={confirmpassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirmer mot de passe"/>
+                      </div>
+
+                  </div>
+
+                  <div className="form_login row actions">
+
+                      <div className="col-10 action">
+                          <input onClick={changePassword} type="button" value={loadingPasswordDatas ? 'Changement de mot de passe...' : 'Changer'}/>
+                      </div>
+
+                  </div>
+
+              </form>
+
+            </div>
+
+          </div>
+
         </Typography>
       </Main>
     </Box>
